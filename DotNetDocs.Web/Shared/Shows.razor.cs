e319d55aa9968d92f398c0eca2e6abecb3ba76d8
David Pine <david.pine@microsoft.com>
2020-06-03 15:59:10 -0500

    Updates to adjust login state

---
+++ DotNetDocs.Web/Shared/Shows.razor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DotNetDocs.Services;
+using DotNetDocs.Services.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace DotNetDocs.Web.Shared
+{
+    public class ShowsComponent : ComponentBase
+    {
+        [Inject]
+        public IScheduleService? ScheduleService { get; set; }
+
+        public IEnumerable<DocsShow> Shows { get; set; } = null!;
+
+        protected override async Task OnInitializedAsync()
+        {
+            if (ScheduleService != null)
+            {
+                Shows = await ScheduleService.GetAllAsync(DateTime.Now.AddYears(-10));
+            }
+        }
+    }
+}
