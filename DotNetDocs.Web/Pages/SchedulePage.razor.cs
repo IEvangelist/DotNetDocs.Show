3073c5c505c4f63db15927735eac9d6fc0f821f3
David Pine <david.pine@microsoft.com>
2020-07-07 12:41:38 -0500

    Added simple schedule page

---
+++ DotNetDocs.Web/Pages/SchedulePage.razor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DotNetDocs.Services;
+using DotNetDocs.Services.Models;
+using DotNetDocs.Web.Workers;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace DotNetDocs.Web.Pages
+{
+    public partial class SchedulePage
+    {
+        [Inject]
+        public NavigationManager Navigation { get; set; } = null!;
+
+        [Inject]
+        public IMemoryCache? Cache { get; set; }
+
+        [Inject]
+        public IScheduleService? ScheduleService { get; set; }
+
+        [Inject]
+        public DateTimeService DateTimeService { get; set; } = null!;
+
+        public IEnumerable<DocsShow>? Shows { get; private set; }
+
+        bool _orderDescending = true;
+
+        protected override async Task OnInitializedAsync()
+        {
+            if (Cache != null && ScheduleService != null)
+            {
+                var utcNow = DateTime.UtcNow;
+                var centralTimeNow = DateTimeService.ConvertFromUtc(utcNow);
+                var shows = await Cache.GetOrCreateAsync(
+                    CacheKeys.ShowSchedule,
+                    async _ =>
+                    await ScheduleService!.GetAllAsync(centralTimeNow.AddYears(-1)));
+                
+                var segmentedShows = DateTimeService.GetSegmentedShows(
+                    shows.Where(show => show.IsPublished),
+                    utcNow,
+                    true);
+
+                Shows = segmentedShows.FutureShows;
+            }
+        }
+    }
+}
