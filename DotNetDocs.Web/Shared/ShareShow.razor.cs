951647327b844d65b79a26e0a6e3bae9113333c2
IEvangelist <david.pine.7@gmail.com>
2020-07-12 15:20:11 -0500

    Added new styling...

---
+++ DotNetDocs.Web/Shared/ShareShow.razor.cs
@@ -0,0 +1,11 @@
+﻿using DotNetDocs.Services.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace DotNetDocs.Web.Shared
+{
+    public partial class ShareShow
+    {
+        [Parameter]
+        public DocsShow? Show { get; set; }
+    }
+}
